ab7e05cd611fb3c2ebf0e8688232a1335597b92a
Richard Hallett <richard@richardhallett.com>
2022-03-09 21:56:39 +0100

    Update examples to use new script

--- views/_data/config.js
+++ views/_data/config.js
@@ -1,8 +1,9 @@
 module.exports = function () {
     return {
         environment: process.env.ELEVENTY_ENV || "development",
-        data_domain: process.env.DATA_DOMAIN || "localhost:5500",
+        repo_id: process.env.DATA_DOMAIN || "demo-site",
         tracking_script_url: process.env.TRACKING_SCRIPT_LOC || "http://localhost:8000/js/custom_tracker/datacite-analytics.js",
+        tracking_api_endpoint: process.env.TRACKING_API_ENDPOINT || "https://analytics.stage.datacite.org",
         dashboard_url: process.env.DASHBOARD_URL || "https://analytics.stage.datacite.org/demorepo.stage.datacite.org",
         dashboard_url_fabrica: process.env.DASHBOARD_URL_FABRICA || "https://doi.stage.datacite.org/repositories/datacite.demo/analytics"
     };
